b6c0d895dc0812418da050ba2223e398f0cdd224
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2019-01-30 16:25:12 -0600

    add Routes to app.js and material ui to set up for navbar

--- client/src/components/App/App.js
+++ client/src/components/App/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import logo from './logo.png'
 import './App.css';
+import {BrowserRouter, Route} from 'react-router-dom';
+
+const PageOne = () => {
+  return <div>PageOne</div>;
+}
+
+const PageTwo = () => {
+  return <div>PageTwo</div>;
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <img src={logo} alt="logo" />
+          <img src={logo} alt="logo" height="50vh" width="50vmin"/>
+          <BrowserRouter>
+            <div>
+              <Route path="/" exact component={PageOne} />
+              <Route path="/pagetwo" exact component={PageTwo} />
+
+            </div>
+          </BrowserRouter>
       </div>
     );
   }
